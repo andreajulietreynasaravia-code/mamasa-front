8f43950b68bd97fd70c76d10fe4a63b6b6147335
Andrea <reynajuli445@gmail.com>
2026-04-19 20:37:38 -0500

    Nueva prueba DevOps

--- test.js
+++ test.js
@@ -4,3 +4,4 @@ console.log("Prueba 1: entorno funcionando ✔");
 console.log("Prueba 2: workflow ejecutado ✔");
 
 console.log("Todas las pruebas pasaron correctamente");
+console.log("Ejecutando nueva prueba DevOps");
